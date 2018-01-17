0321e1d02455d212de0f10408986327245271fdf
Ramirema <mathias.ramirez.gallardo@hotmail.com>
2018-01-17 09:45:34 +0100

    exe04

--- exe04/routes/about.js
+++ exe04/routes/about.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 module.exports = (req,res) =>{
-    fs.readFile(`${process.cwd()} /templates/about.html`, {encoding: 'utf-8'} , (err,data) =>{
+    console.log('ok')
+    fs.readFile(`${process.cwd()}/templates/about.html`, { encoding: 'utf-8' }, (err, data) => {
         res.end(data)
     })
 }
--- exe04/server.js
+++ exe04/server.js
@@ -1,23 +1,18 @@
 const http = require('http')
-const fs = require('fs')
-
 
 const routes = [
-    {url:'/', controller : 'home'},
-    {url:'/about', controller : 'about'},
-    {url:'/amis', controller : 'amis'}
+    { url: '/', controller: 'home' },
+    { url: '/about', controller: 'about' },
+    { url: '/amis', controller: 'amis' }
 ]
-const router = (req,res) => {
+const router = (req, res) => {
     let indexRoute = routes.findIndex(item => item.url === req.url)
-    if(indexRoute !== -1){
+    if (indexRoute !== -1) {
         require(`./routes/${routes[indexRoute].controller}`)(req,res)
-    }else{
-        require(`./routes/error`)(req,res)
+    } else {
+        require('./routes/error')(req,res)
     }
 }
-
-
-
-http.createServer(router).listen(8000 ,()=>{
+http.createServer(router).listen(8000, () => {
     console.log('server running and listening port 8000')
 })
